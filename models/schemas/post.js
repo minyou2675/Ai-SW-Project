145ade42049fb65ace6298c4049ce4bb56d8f21e
uneducated-me <88090335+uneducated-me@users.noreply.github.com>
2022-08-18 10:13:00 +0900

    Add files via upload

---
+++ models/schemas/post.js
@@ -0,0 +1,11 @@
+const { Schema } = require("mongoose");
+const shortId = require('./type/short-id');
+
+module.exports = new Schema({
+    shortId,
+    img: String,
+    
+}, {
+    timestamps: true
+}
+);
